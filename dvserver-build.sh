49fa5fad57c7c26b4c20d23c3634846afcdb40a4
Niklas Schmidtmer <niklas.schmidtmer@datavirtuality.com>
2020-07-13 13:18:12 +0200

    Improve variable naming and usage formatting

--- dvserver-build.sh
+++ dvserver-build.sh
@@ -3,18 +3,18 @@
 set -e
 
 if [[ $# -eq 0 || $# -ge 3 ]]; then
-    echo "    Use: dvserver-build.sh <img_to_run:tag> <arg_dvserver_ver_to_deploy>"
-    echo "         Parameter <image:tag> is mandatory."
-    echo "    Use: dvserver-build.sh dvserver:2.3.10 dvserver-TRUNK.linux.x86_64.zip"
+    echo "Useage: dvserver-build.sh <img_to_build:tag> <arg_dvserver_ver_to_deploy>"
+    echo "        Parameter <image:tag> is mandatory."
+    echo "Example: ./dvserver-build.sh dvserver:2.3.10 dvserver-TRUNK.linux.x86_64.zip"
     exit 1
 elif [[ $# -eq 1 ]]; then
-    img_to_run=$1
+    img_to_build=$1
     arg_dvserver_ver_to_deploy=dvserver-TRUNK.linux.x86_64.zip
 elif [[ $# -eq 2 ]]; then
-    img_to_run=$1
+    img_to_build=$1
     arg_dvserver_ver_to_deploy=$2
 fi
 
 unzip "$arg_dvserver_ver_to_deploy"
-docker build -t "$img_to_run" .
+docker build -t "$img_to_build" .
 rm -rf dvserver
